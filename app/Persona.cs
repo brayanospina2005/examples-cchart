4085e86f5fc24c3594770e6504327b62cae3210b
Administrador <Administrador@B13-203-4989>
2023-02-20 10:39:52 -0500

    Agregar archivos de proyecto.

---
+++ app/Persona.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public class Persona
+    {
+        //atributos
+        private string apellido;
+        private byte edad;
+        private double altura;
+        private string nombre;
+        private double peso;
+
+        public Persona(string nombre, string apellido, byte edad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        public Persona(string apellido,string nombre, double peso, byte edad, double altura)
+        {
+            this.Apellido = apellido;
+            this.Edad = edad;
+            this.Altura = altura;
+            this.nombre = nombre;
+            this.Peso = peso;
+          
+        }
+        public Persona (string nombre, double peso)
+        {
+            this.nombre = nombre;
+            this.peso = peso;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public byte Edad { get => edad; set => edad = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public double Altura { get => altura; set => altura = value; }
+
+        //metodos
+        public bool Esmayor()
+        {
+            if ( edad >= 18)
+                 return true;
+            else
+                 return false;   
+        }
+
+        public int CalcularImc()
+        {
+            float imc = (float)(peso / (Math.Pow(altura, 2)));
+            if (imc <= 18.5)
+                return 0;
+            else if (imc > 18.5 && imc <= 24.9)
+                return 1;
+            else
+                return 2;
+        }
+
+    }
+}
---
+++ app/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using app;
+using System;
+
+public
+    
+    class HelloWorld
+{
+
+    public static void Main(string[] args)
+    {
+        Persona persona1 = new Persona("",15);
+        persona1.Nombre = "Brayan";
+        persona1.Edad = 17;
+        persona1.Peso = 60.5d;
+        persona1.Altura = 1.72d;
+        Console.Write("la persona:{0} ", persona1.Nombre);
+        if (persona1.Esmayor())
+            Console.WriteLine("es mayor de edad");
+        else
+            Console.WriteLine("es menor de edad");
+        int respuesta = persona1.CalcularImc();
+        if (respuesta == 0)
+            Console.WriteLine("la persona esta baja de peso");
+        else if (respuesta == 1)
+            Console.WriteLine("La persona tiene peso normal");
+        else
+            Console.WriteLine("la persona tiene sobrepeso");
+
+
+        
+         Console.WriteLine("hola");
+
+        Persona car = new Persona("brayan", "ospina", 60, 17, 1.72);
+        Console.WriteLine(car.);
+       
+    }
+}
